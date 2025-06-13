e6c1a9bdd7ce22f6ca53ed0db6638544941f5b26
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-13 19:51:48 +0530

    solar

--- myapp/task.py
+++ myapp/task.py
@@ -2,7 +2,7 @@ from time import sleep
 from celery import shared_task
 from mycelery.celery import app
 from datetime import timedelta
-from celery.schedules import crontab
+from celery.schedules import crontab,solar
 
 # Create a Celery instance
 @shared_task(bind=True, name='myapp.tasks.add')
@@ -33,10 +33,47 @@ def sub(self, x, y):
 #         'args': (11,),  # Arguments to pass to the task
 #     }
 # }
+# app.conf.beat_schedule = {
+#     'clear_cache_every_10_seconds': {
+#         'task': 'myapp.task.clear_cache',  # The task to run
+#         'schedule': crontab(minute='*/1'),  # Run every 10 seconds
+#         'args': (11,),  # Arguments to pass to the task
+#     }
+# }
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender: Celery, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+#     # Calls test('hello') every 30 seconds.
+#     # It uses the same signature of previous task, an explicit name is
+#     # defined to avoid this task replacing the previous one defined.
+#     sender.add_periodic_task(30.0, test.s('hello'), name='add every 30')
+
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+#     # Executes every Monday morning at 7:30 a.m.
+#     sender.add_periodic_task(
+#         crontab(hour=7, minute=30, day_of_week=1),
+#         test.s('Happy Mondays!'),
+#     )
+
+# @app.task
+# def test(arg):
+#     print(arg)
+
+# @app.task
+# def add(x, y):
+#     z = x + y
+#     print(z)
+
 app.conf.beat_schedule = {
-    'clear_cache_every_10_seconds': {
-        'task': 'myapp.task.clear_cache',  # The task to run
-        'schedule': crontab(minute='*/1'),  # Run every 10 seconds
-        'args': (11,),  # Arguments to pass to the task
-    }
+    # Executes at sunset in Melbourne
+    'add-at-melbourne-sunset': {
+        'task': 'tasks.add',
+        'schedule': solar('sunset', -37.81753, 144.96715),
+        'args': (16, 16),
+    },
 }
