569fbb8971c56171dad55db66eb4528631594623
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-13 19:40:47 +0530

    crontab

--- myapp/task.py
+++ myapp/task.py
@@ -1,7 +1,8 @@
 from time import sleep
 from celery import shared_task
 from mycelery.celery import app
-
+from datetime import timedelta
+from celery.schedules import crontab
 
 # Create a Celery instance
 @shared_task(bind=True, name='myapp.tasks.add')
@@ -15,10 +16,27 @@ def sub(self, x, y):
 #     return id
 
 #   # A task to clear the cache method 2
+# app.conf.beat_schedule = {
+#     'clear_cache_every_10_seconds': {
+#         'task': 'myapp.task.clear_cache',  # The task to run
+#         'schedule': 10.0,  # Run every 10 seconds
+#         'args': (1,),  # Arguments to pass to the task
+#     }
+# }
+
+#  # A task to clear using timedelta
+
+# app.conf.beat_schedule = {
+#     'clear_cache_every_10_seconds': {
+#         'task': 'myapp.task.clear_cache',  # The task to run
+#         'schedule': timedelta(seconds=10),  # Run every 10 seconds
+#         'args': (11,),  # Arguments to pass to the task
+#     }
+# }
 app.conf.beat_schedule = {
     'clear_cache_every_10_seconds': {
         'task': 'myapp.task.clear_cache',  # The task to run
-        'schedule': 10.0,  # Run every 10 seconds
-        'args': (1,),  # Arguments to pass to the task
+        'schedule': crontab(minute='*/1'),  # Run every 10 seconds
+        'args': (11,),  # Arguments to pass to the task
     }
 }
